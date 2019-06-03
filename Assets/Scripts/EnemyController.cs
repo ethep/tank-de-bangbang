610d00b5991fbcc1644e999da59b66c28b323778
etheP <ethep.amp@gmail.com>
2019-06-03 14:46:15 +0900

    敵用モデルを追加して、ランダムにスポーンさせる
    ついでに、画面の調整

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    private void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
